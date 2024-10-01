f9e3fc7dbd1d7dd8d648c0b2f306bab35afdd22b
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-01 15:25:17 +0900

    02_03完了

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
+int main(int argc,char* argv[]) {
 
-	printf("Hello,World!\n");
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
 
 	system("pause");
